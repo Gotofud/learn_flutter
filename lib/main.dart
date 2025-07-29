cdfea9ae1a398eb1f143316263e27cecf2f5790f
Gotofud <pajlisukabuah@gmail.com>
2025-07-29 16:56:12 +0700

    Latihan 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/container_widget/container_dua.dart';
 import 'package:learn_flutter/container_widget/container_satu.dart';
+import 'package:learn_flutter/latihan/latihan_satu.dart';
+import 'package:learn_flutter/latihan/roblox_card/roblox_card.dart';
 import 'package:learn_flutter/main_layout.dart';
+import 'package:learn_flutter/row_column_widget/column_satu.dart';
+import 'package:learn_flutter/row_column_widget/latihan_row_column.dart';
+import 'package:learn_flutter/row_column_widget/row_satu.dart';
 import 'package:learn_flutter/stack_widget/stack_dua.dart';
 import 'package:learn_flutter/stack_widget/stack_satu.dart';
 import 'package:learn_flutter/stack_widget/stack_tiga.dart';
@@ -19,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StackTwo(),
+      home: RobloxCard()
     );
   }
 }
